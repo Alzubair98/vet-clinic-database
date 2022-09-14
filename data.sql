8d14ff15466826093b83be4bd046a5988e89662f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-09-15 00:44:23 +0300

    QUERS& DATA

--- data.sql
+++ data.sql
@@ -1,11 +1,32 @@
-INSERET INTO animals(name, date_of_birth, escape_attmepts,neutered,weight_kg)
+INSERT INTO animals(name, date_of_birth, escape_attmepts,neutered,weight_kg)
 VALUES('Agumon','2022-02-03',0,b'1',10.23);
 
-INSERET INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
 VALUES('Gabumon','2018-11-15',2,b'1',8);
 
-INSERET INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
 VALUES('Pikachu','2021-01-07',1,b'1',15.04);
 
-INSERET INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
 VALUES('Devimon','2017-05-12',5,b'1',11);
+
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+VALUES('Charmander','2020-02-08',0,b'0',11);
+
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+VALUES('Plantmon','2021-11-15',2,b'1',5.7);
+
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+VALUES('Squirtle', '1993-04-02', 3, b'0', 12.13);
+
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+VALUES('Angemon', '2005-06-12', 1, b'1', 45);
+
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+VALUES('Boarmon', '2005-06-07', 7, b'1', 20.4);
+
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+VALUES('Blossom', '1998-10-13', 3, b'1', 17);
+
+INSERT INTO animals(name, date_of_birth, escape_attmepts, neutered, weight_kg)
+VALUES('Ditto', '1998-05-14', 4, b'1', 22);
